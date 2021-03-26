83591e643bc36c88514366167acfc74b79b7f4ba
stefiya <stefiyajoy94@gmail.com>
2021-03-26 18:15:34 +0530

    first commit

---
+++ basic/js/script.js
@@ -0,0 +1,19 @@
+/*element selector
+function demo()
+{
+	$("h1").fadeToggle();
+}*/
+
+/*function demo()
+{
+	$("#id1").fadeToggle();
+}
+*/
+
+/*
+function demo()
+{
+	$(".id2").fadeToggle();
+}
+*/
+
